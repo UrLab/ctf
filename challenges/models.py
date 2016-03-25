b75033fb5ca09a1299bbb3abc0b86ec431c448e5
Romain Fontaine <romain-fontaine@outlook.com>
2016-03-25 16:35:34 +0100

    create app for challenges

--- challenges/models.py
+++ challenges/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=1000)
+
+class Challenge(models.Model):
+    title = models.CharField(max_length=1000)
+    decription = models.TextField()
+    attachment = models.FileField(upload_to="attachments/", blank=True, null=True)
+    category = models.ForeignKey('Category')
+    points = models.IntegerField(min_value=0, max_value=65535)
+
+class Hint(models.Model):
+    text = models.TextField()
+    challenge = models.ForeignKey('Challenge')
