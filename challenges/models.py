ba474da5c8d9954775495a9fc8a4b69ca4a5ce11
Nikita Marchant <nikita.marchant@gmail.com>
2016-12-22 11:53:35 +0100

    Add visibility to hints

--- challenges/models.py
+++ challenges/models.py
@@ -1,15 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
 class Category(models.Model):
     name = models.CharField(max_length=1000)
+
+    class Meta:
+        verbose_name_plural = "categories"
 
     def __str__(self):
         return self.name
 
+
 class Challenge(models.Model):
     title = models.CharField(max_length=1000)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     attachment = models.FileField(upload_to="attachments/", blank=True, null=True)
     category = models.ForeignKey('Category')
     points = models.PositiveSmallIntegerField()
@@ -18,13 +22,23 @@ class Challenge(models.Model):
     def __str__(self):
         return self.title
 
+
 class Hint(models.Model):
     text = models.TextField()
     challenge = models.ForeignKey('Challenge')
+    visible = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "Hint for %s" % self.challenge
+
 
 class Resolution(models.Model):
     challenge = models.ForeignKey('Challenge')
     team = models.ForeignKey('users.Team')
     time = models.DateTimeField(auto_now=True)
+
     class Meta:
         unique_together = (("challenge", "team"))
+
+    def __str__(self):
+        return "Resolution of %s from %s" % (self.challenge, self.team)
