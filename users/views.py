d4ad068fec5ef2078fdd8650c56bbae658524ec1
Nikita Marchant <nikita.marchant@gmail.com>
2016-12-23 21:42:45 +0100

    Add team join logic

--- users/views.py
+++ users/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 from .forms import UserForm
-from .models import User
-from django.http import HttpResponseRedirect
+from .models import User, Team
+from django.http import HttpResponseRedirect, HttpResponseForbidden
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+from django.contrib import messages
 
 from users.decorators import team_required
 
@@ -34,4 +36,19 @@ def show_team(request):
 
 @login_required
 def accept_invite(request, secret_key):
-    return None
+    if not secret_key or len(secret_key) < 10:
+        return HttpResponseForbidden("Wrong key format.")
+    team = get_object_or_404(Team, secret_key=secret_key)
+
+    if request.method == 'POST':
+        if team.members.count() >= 4:
+            return HttpResponseForbidden("Team is full, sorry.")
+        if request.user.team:
+            return HttpResponseForbidden("You already have a team.")
+
+        messages.add_message(request, messages.SUCCESS, "You joined %s. Welcome on board!" % team)
+        request.user.team = team
+        request.user.save()
+        return HttpResponseRedirect(reverse('team'))
+    else:
+        return render(request, 'users/accept_invite.html', {'team': team})
