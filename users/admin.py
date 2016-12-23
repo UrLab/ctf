988efd50a0a9267fba4838bf826a7dad6a4292e0
Nikita Marchant <nikita.marchant@gmail.com>
2016-12-23 22:22:50 +0100

    Add a proof token

--- users/admin.py
+++ users/admin.py
@@ -7,6 +7,7 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'team', 'email', 'created', 'first_name', 'last_name', 'affiliation')
     list_filter = ('team', 'affiliation')
     search_fields = ('username', 'email', 'first_name', 'last_name')
+    readonly_fields = ('proof_token', )
 
 
 @admin.register(Team)
